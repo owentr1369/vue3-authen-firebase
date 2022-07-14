e2a6ee5b62057554cb7f82cbad067789702aff0c
TranTam <tamvaa1306@gmail.com>
2022-07-14 17:27:58 +0700

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,8 +2,20 @@
   <div class="login">
     <h1>Login</h1>
     <form @submit.prevent="Login">
-      <input type="text" name="" id="" placeholder="Your email" />
-      <input type="password" name="" id="" placeholder="Password" />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Your email"
+        v-model="email"
+      />
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="Password"
+        v-model="password"
+      />
       <input type="submit" value="Login" />
       <p>
         Need an account?
@@ -14,7 +26,27 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import firebase from "firebase/app";
+
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const Login = () => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value)
+        .then((data) => console.log("data", data))
+        .catch((err) => console.log(err));
+    };
+    return {
+      Login,
+      email,
+      password,
+    };
+  },
+};
 </script>
 
 <style>

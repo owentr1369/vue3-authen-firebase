ea737973d50c877b67cbbb0abce0c56c0bdc72f7
TranTam <tamvaa1306@gmail.com>
2022-07-14 17:28:06 +0700

    register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,11 +1,51 @@
 <template>
   <div class="register">
     <h1>Register</h1>
+    <form @submit.prevent="Register">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Your email"
+        v-model="email"
+      />
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="Password"
+        v-model="password"
+      />
+      <input type="submit" value="Register" />
+      <p>
+        Had an account?
+        <router-link :to="{ name: 'login' }">Login</router-link>
+      </p>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import firebase from "firebase/app";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const Register = () => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email.value, password.value)
+        .then((data) => console.log("data", data))
+        .catch((err) => console.log(err));
+    };
+    return {
+      Register,
+      email,
+      password,
+    };
+  },
+};
 </script>
 
 <style>

b32216e448382ea1ca8db46ac95e69f4e1163235
TranTam <tamvaa1306@gmail.com>
2022-07-14 16:54:46 +0700

    config firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase";
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyDCJaxGqw1OrlbEIfkY7hEFxLvPN4xPQO8",
+  authDomain: "vue3-authen-firebase.firebaseapp.com",
+  projectId: "vue3-authen-firebase",
+  storageBucket: "vue3-authen-firebase.appspot.com",
+  messagingSenderId: "942237333566",
+  appId: "1:942237333566:web:f2b02a3a1f45b20217396f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+createApp(App).use(router).mount("#app");

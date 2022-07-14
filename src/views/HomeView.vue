bc1a372b3585b7f71adc3d051362487bf8de9092
TranTam <tamvaa1306@gmail.com>
2022-07-14 17:28:17 +0700

    welcome and logout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <h2>Welcome back, {{ name }}</h2>
+    <router-link :to="{ name: 'about' }">About</router-link>
+    <button class="logout" @click="Logout">Logout</button>
   </div>
 </template>
 
 <script>
+import { ref, onBeforeMount } from "vue";
+import firebase from "firebase/app";
 export default {
   name: "HomeView",
+  setup() {
+    const name = ref("");
+    onBeforeMount(() => {
+      const user = firebase.auth().currentUser;
+      if (user) {
+        name.value = user.email.split("@")[0];
+      }
+    });
+    const Logout = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => console.log("Signed Out"))
+        .catch((err) => console.log(err));
+    };
+    return {
+      name,
+      Logout,
+    };
+  },
 };
 </script>
